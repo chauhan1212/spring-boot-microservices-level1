e7b2c184a8e855bbf63c026333807a67c4c387fc
vijay chauhan <chauhanvijay81@gmail.com>
2022-08-06 13:12:37 +0530

    Video 13 about WebFlux for call external APIs

--- movie-catalog-service/src/main/java/com/vijay/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/vijay/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,8 +2,10 @@ package com.vijay.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 //server.port=8081
 @SpringBootApplication
@@ -13,6 +15,11 @@ public class MovieCatalogServiceApplication {
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
 	};
+	
+	@Bean
+	public WebClient.Builder getWebClientBuilder() {
+		return WebClient.builder();
+	}
 
 	//API: http://localhost:8081/catalog/123
 	public static void main(String[] args) {
--- movie-catalog-service/src/main/java/com/vijay/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/vijay/moviecatalogservice/resources/MovieCatalogResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.vijay.moviecatalogservice.models.CatalogItem;
 import com.vijay.moviecatalogservice.models.Movie;
@@ -24,6 +25,9 @@ public class MovieCatalogResource {
 	
 	@Autowired
 	private RestTemplate restTemplate;
+	
+	@Autowired
+	private WebClient.Builder webClientBuilder;
 
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
@@ -31,7 +35,13 @@ public class MovieCatalogResource {
 		List<Rating> ratings = Arrays.asList(new Rating("1234", 4), new Rating("5678", 3));
 
 		List<CatalogItem> catalogs = ratings.stream().map(rating -> {
-			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+			//Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+			Movie movie = webClientBuilder.build()
+				.get()
+				.uri("http://localhost:8082/movies/" + rating.getMovieId())
+				.retrieve()
+				.bodyToMono(Movie.class)
+				.block();
 			return new CatalogItem(movie.getMovieId(), movie.getDescription(), rating.getRating());
 		}).collect(Collectors.toList());
 
